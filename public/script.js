e644fbe806e11c748dee1cab0c784edf412511d2
Swornima <sworstha@gmail.com>
2021-12-12 08:34:52 +0545

    Displayed data in webpage.

--- public/script.js
+++ public/script.js
@@ -1,14 +1,14 @@
 const button = document.getElementsByTagName("button")[0];
+
 button.addEventListener("click", async (e) => {
     //prevent page from reloading after submitting.
     e.preventDefault();
+    const weather = document.getElementById("weather");
+    const location = document.getElementById("place");
     const place = document.getElementById("location").value;
     const data = await (await fetch("/weather?Location=" + place)).json();
     const { place_name, temperature, description, wind_degree, humidity } =
         data;
-    console.log("Location", place_name);
-    console.log("Temperature", temperature, "degree Celsius");
-    console.log("Description", description);
-    console.log("Wind degree", wind_degree);
-    console.log("Humidity", humidity);
+    location.innerText = ("Location", place_name);
+    weather.innerText = `Temperature ${temperature} degree Celsius, ${description}, Wind-degree ${wind_degree}, Humidity ${humidity}`;
 });
